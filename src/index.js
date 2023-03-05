c4bfeafb9cb42e15837a9e21735269394306d734
adenugbamichael <adenugbamicke@gmail.com>
2023-03-05 16:17:12 +0100

    refactoring from functioning to class components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-  return <div>Latitude: </div>
+    this.state = { lat: null, errorMessage: "" }
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called setstate!!!!
+        this.setState({ lat: position.coords.latitude })
+      },
+
+      (err) => {
+        this.setState({ errorMessage: err.message })
+      }
+    )
+  }
+
+  // React says we have to define render
+  render() {
+    if (this.state.errorMesaage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading!</div>
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
